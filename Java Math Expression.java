0cb0596f74b57c08fd87069398d76dbf50fba53c
Suraj Yadav <136815807+SurajYadav69@users.noreply.github.com>
2023-09-11 14:55:00 +0530

    Create Java Math Expression.java

---
+++ Java Math Expression.java
@@ -0,0 +1,14 @@
+public class MathExpression {
+    public static void main(String[] args) {
+        int numerator1 = 7;
+        int denominator1 = 4;
+        int numerator2 = 9;
+        int denominator2 = 2;
+
+        // Calculate the product of the fractions
+        int productNumerator = numerator1 * numerator2;
+        int productDenominator = denominator1 * denominator2;
+
+        System.out.println("Result: " + productNumerator + "/" + productDenominator);
+    }
+}
